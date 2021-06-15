ce65ec5ed6839768c639303f39c91a73ac109a39
ferchu1999 <71432893+ferchu1999@users.noreply.github.com>
2021-06-15 10:56:30 +0200

    Update settings.py

--- ProyectoFinal/settings.py
+++ ProyectoFinal/settings.py
@@ -140,7 +140,7 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "ferranguillem8@gmail.com"
-EMAIL_HOST_PASSWORD = "Taekwondo9"
+EMAIL_HOST_PASSWORD = "*******"
 
 
 LOGIN_REDIRECT_URL = '/'
